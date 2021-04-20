e17ec10a4161281598b9ab30d732e566e884ae6f
Samantha <Sambollano54@gmail.com>
2021-04-20 13:27:25 -0500

    /

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
